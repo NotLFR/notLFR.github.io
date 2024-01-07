89298e74b1227509c0fe523801ee51e297bf3b9c
NotLFR <37275306+NotLFR@users.noreply.github.com>
2024-01-07 12:46:22 -0500

    Revert "Revert "Init commit""
    This reverts commit 8889b3a0d4ed55edb15c5f5bb9b4f46bc371ac36.

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useRef } from 'react';
+import moon from '/Users/oconn/parallax/src/moon.jpg'
+import land from '/Users/oconn/parallax/src/land.jpg'
+import { Parallax, ParallaxLayer } from "@react-spring/parallax"
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
+
+
+function App() {
+  const ref = useRef();
+
+  return (
+    <div>
+      <Parallax pages={4} ref={ref}>
+        {}
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: 'cover',
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Welcome!</h2>
+          <h3>by Gannon O'Connor</h3>
+          <h4>Click to take a journey</h4>
+          
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>About me!</h2>
+          <p>Welcome to my new and improved website.</p>
+        <p>I am currently employed by <a href="https://www.firstlight.net/" target='blank'>Firstlight</a></p>
+         <p>To book me on Calendly click the emoji <a href="https://calendly.com/g_oconnor/30min" >🗓️</a></p> 
+         <br />
+         <p><a href="https://docs.google.com/document/d/e/2PACX-1vQavsygp5SQMpwX5qmSjj23pWbN2760NN-w_X-4tLv7KMPzCcrnUHlzRSIcc_k92Q/pub" target='blank'>Résumé</a></p>
+         <p><a href="https://www.linkedin.com/in/gannon-o-connor-593871161/" target='blank'>LinkedIn</a></p>
+        <br />
+        <p>Shoot For The Stars 💫</p>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+}
+
+export default App;
